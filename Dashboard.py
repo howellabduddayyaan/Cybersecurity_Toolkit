afa975c347cc34a2bff31b844bc3b6a532b86e08
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-30 20:27:38 +0200

    Feat : Added all the functions I would like to include in my Dashboard

---
+++ Dashboard.py
@@ -0,0 +1,83 @@
+# =============================
+# === Cybersecurity Toolkit ===
+# =============================
+
+'''
+Main Dashboard
+
+Run : python dashboard.py
+'''
+
+import os
+
+# _________________________________________________________________________________________________
+
+# --------------
+# Tool Functions
+# --------------
+
+def device_scanner():
+    print("\nLaunching Device Scanner...")
+    
+
+def network_scanner():
+    print("\nLaunching Network Scanner...")
+
+
+def port_scanner():
+    print("\nLaunching Port Scanner...")
+
+
+def packet_sniffer():
+    print("\nLaunching Packet Sniffer...")
+
+
+def dns_lookup():
+    print("\nLaunching DNS Lookup...")
+
+
+def phishing_checker():
+    print("\nLaunching Phishing URL Checker...")
+
+
+def ssl_checker():
+    print("\nLaunching SSL Certificate Checker...")
+
+
+def whois_lookup():
+    print("\nLaunching WHOIS Lookup...")
+
+
+def login_system():
+    print("\nLaunching Login System...")
+
+
+def password_manager():
+    print("\nLaunching Password Manager...")
+
+
+def password_strength():
+    print("\nLaunching Password Strength Checker...")
+
+
+def file_hash():
+    print("\nLaunching File Hash Checker...")
+
+
+def caesar_cipher():
+    print("\nLaunching Caesar Cipher...")
+
+
+def base64_tool():
+    print("\nLaunching Base64 Encoder / Decoder...")
+
+
+def log_analyzer():
+    print("\nLaunching Log Analyzer...")
+
+
+def brute_force():
+    print("\nLaunching Brute Force Simulator...")
+
+# _________________________________________________________________________________________________
+
