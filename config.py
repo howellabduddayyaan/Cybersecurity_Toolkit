71087ace1870ea0e840380560323653f68b5a030
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-03 21:10:10 +0200

    Feat : Added storage vault for txt files in Config file

---
+++ config.py
@@ -0,0 +1,33 @@
+# =====================
+# === Configuration ===
+# =====================
+
+from pathlib import Path
+
+# -------------------
+# Project Information
+# -------------------
+
+APP_NAME = "Cybersecurity Toolkit"
+VERSION = "1.0.0"
+AUTHOR = "Abdud Dayyaan Howell"
+
+# -------------
+# Project Paths
+# -------------
+
+BASE_DIR = Path(__file__).resolve().parent
+
+ASSETS_DIR = BASE_DIR / "assets"
+EXPORTS_DIR = ASSETS_DIR / "exports"
+
+# Storage Vault
+
+SAMPLE_DOMAINS = ASSETS_DIR / "sample_domains.txt"
+TEST_URLS = ASSETS_DIR / "test_urls.txt"
+SAMPLE_LOGS = ASSETS_DIR / "sample_logs.txt"
+COMMON_PASSWORDS = ASSETS_DIR / "common_passwords.txt"
+
+USERS_FILE = BASE_DIR / "users.txt"
+VAULT_FILE = BASE_DIR / "vault.txt"
+
