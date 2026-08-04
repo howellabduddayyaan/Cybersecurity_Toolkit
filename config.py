15c9afce2ed7371a339e190e2487c48f5a363a31
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-04 20:33:24 +0200

    Feat : Added Url Risk scoring and suspicious keywords

--- config.py
+++ config.py
@@ -69,34 +69,56 @@ PROGRESS_BAR_WIDTH = 30
 # Ports
 # -----
 
-COMMON_PORTS = {20 : "FTP Data",
-                21 : "FTP",
-                22 : "SSH",
-                23 : "Telnet",
-                25 : "SMTP",
-                53 : "DNS",
-                67 : "DHCP",
-                68 : "DHCP",
-                69 : "TFTP",
-                80 : "HTTP",
-                110 : "POP3",
-                123 :"NTP",
-                143 : "IMAP",
-                161 : "SNMP",
-                389 : "LDAP",
-                443 : "HTTPS",
-                445 : "SMB",
-                587 : "SMTP TLS",
-                993 : "IMAPS",
-                995 : "POP3S",
-                1433 : "MSSQL",
-                1521 : "Oracle",
-                3306 : "MySQL",
-                3389 : "RDP",
-                5432 : "PostgreSQL",
-                5900 : "VNC",
-                6379 : "Redis",
-                8080 : "HTTP Alternate"
-                }
+COMMON_PORTS = {
+    20 : "FTP Data",
+    21 : "FTP",
+    22 : "SSH",
+    23 : "Telnet",
+    25 : "SMTP",
+    53 : "DNS",
+    67 : "DHCP",
+    68 : "DHCP",
+    69 : "TFTP",
+    80 : "HTTP",
+    110 : "POP3",
+    123 :"NTP",
+    143 : "IMAP",
+    161 : "SNMP",
+    389 : "LDAP",
+    443 : "HTTPS",
+    445 : "SMB",
+    587 : "SMTP TLS",
+    993 : "IMAPS",
+    995 : "POP3S",
+    1433 : "MSSQL",
+    1521 : "Oracle",
+    3306 : "MySQL",
+    3389 : "RDP",
+    5432 : "PostgreSQL",
+    5900 : "VNC",
+    6379 : "Redis",
+    8080 : "HTTP Alternate"
+    }
 
 # _________________________________________________________________________________________________
+
+# ----------------
+# URL Risk Scoring
+# ----------------
+
+SUSPICIOUS_KEYWORDS = [
+    "login",
+    "verify",
+    "secure",
+    "account",
+    "update",
+    "bank",
+    "password",
+    "confirm"
+    ]
+
+HIGH_RISK_SCORE = 4
+MEDIUM_RISK_SCORE = 2
+
+# _________________________________________________________________________________________________
+
