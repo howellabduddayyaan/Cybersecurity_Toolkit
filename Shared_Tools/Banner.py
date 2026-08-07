325594ae465699c6811e37d057225b02912e4616
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-07 20:11:49 +0200

    Feat : Added Banner.py for the into shared assets (note still issues with the banner layout

---
+++ Shared_Tools/Banner.py
@@ -0,0 +1,11 @@
+# ====================================
+# === Cybersecurity Toolkit Banner ===
+# ====================================
+
+def show_banner(title):
+
+    # print("\n" + "=" * 60)
+    # print("         CYBERSECURITY TOOLKIT")
+    # print("=" * 60)
+    # print(title.center(60))
+    # print("=" * 60)
