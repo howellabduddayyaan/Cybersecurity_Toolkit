41c5a54d0e5406b2eac809bd7ac0ebc324ec0076
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-08 20:27:45 +0200

    Feat : Added Menu.py to display what option the user has picked as well as a back option

--- Shared_Tools/Banner.py
+++ Shared_Tools/Banner.py
@@ -4,8 +4,10 @@
 
 def show_banner(title):
 
-    # print("\n" + "=" * 60)
-    # print("         CYBERSECURITY TOOLKIT")
-    # print("=" * 60)
-    # print(title.center(60))
-    # print("=" * 60)
+    print("\n" + "=" * 60)
+    print("         CYBERSECURITY TOOLKIT")
+    print("=" * 60)
+    print(title.center(60))
+    print("=" * 60)
+    
+# _________________________________________________________________________________________________
---
+++ Shared_Tools/Menu.py
@@ -0,0 +1,14 @@
+# ==================================
+# === Cybersecurity Toolkit Menu ===
+# ==================================
+
+def show_menu(title, options):
+
+    print(f"\n=== {title} ===\n")
+
+    for number, option in enumerate(options, start=1):
+        print(f"{number}. {option}")
+
+    print("0. | Back")
+    
+# _________________________________________________________________________________________________
