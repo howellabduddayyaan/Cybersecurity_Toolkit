b698e4b10502eada6b608625e011faf5eeb76eef
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-08 20:36:44 +0200

    Feat : Added Menu_Utilities.py to prevent rerunning the program over and over

---
+++ Shared_Tools/Menu_Utilities.py
@@ -0,0 +1,18 @@
+# ======================
+# === Menu Utilities ===
+# ======================
+
+import os
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def pause():
+    input("\nPress Enter to continue...")
+
+
+def line():
+    print("-" * 60)
+    
+# _________________________________________________________________________________________________
