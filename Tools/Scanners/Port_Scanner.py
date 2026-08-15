fd2ed0e40aa83a06a58f688ebcd857c0480372e3
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-15 20:18:00 +0200

    Feat : Port_Scanner, added function to scan for open ports and the host

--- Tools/Scanners/Port_Scanner.py
+++ Tools/Scanners/Port_Scanner.py
@@ -8,3 +8,28 @@ from Shared_Tools.Banner import show_banner
 from Shared_Tools.Tables import print_section, print_analysis_complete
 from Shared_Tools.Menu_Utilities import pause
 
+# _________________________________________________________________________________________________
+
+def scan_port(host, port):
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    sock.settimeout(0.5)
+
+    try:
+
+        result = sock.connect_ex((host, port))
+
+        if result == 0:
+
+            print(f"Port {port:<6}: OPEN")
+
+            return True
+
+        return False
+
+    finally:
+
+        sock.close()
+
+# _________________________________________________________________________________________________
