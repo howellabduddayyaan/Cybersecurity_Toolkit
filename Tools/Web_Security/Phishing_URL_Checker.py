a5f23960edf5d9ef715e4e3f13b0428aefe05678
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-17 20:27:53 +0200

    Feat : Web_Security, Phishing_URL_Checker.py Added suspicious keywords

---
+++ Tools/Web_Security/Phishing_URL_Checker.py
@@ -0,0 +1,16 @@
+# ============================
+# === Phishing URL Checker ===
+# ============================
+
+
+
+SUSPICIOUS_KEYWORDS = [
+    "login",
+    "verify",
+    "secure",
+    "account",
+    "update",
+    "bank",
+    "password",
+    "confirm"
+]
