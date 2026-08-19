a7f0e62c71184ac2cdc5593c8573e2041765c761
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-19 21:12:29 +0200

    Feat : Web_Security, Phishing_URL_Checker.py Added function that checks IP Address

--- Tools/Web_Security/Phishing_URL_Checker.py
+++ Tools/Web_Security/Phishing_URL_Checker.py
@@ -2,6 +2,12 @@
 # === Phishing URL Checker ===
 # ============================
 
+import re
+from urllib.parse import urlparse
+
+from Shared_Tools.Banner import show_banner
+from Shared_Tools.Tables import print_section, print_analysis_complete
+from Shared_Tools.Menu_Utilities import pause
 
 
 SUSPICIOUS_KEYWORDS = [
@@ -14,3 +20,13 @@ SUSPICIOUS_KEYWORDS = [
     "password",
     "confirm"
 ]
+
+def is_ip(address):
+
+    return re.match(
+        r"^\d{1,3}(\.\d{1,3}){3}$",
+        address
+    ) is not None
+
+# _________________________________________________________________________________________________
+
