af83620190280f8afb64ced2c6606c8b8694f419
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-26 19:51:51 +0200

    Feat : Web_Security, Whois_lookup (created file)

---
+++ Tools/Web_Security/WhoIs_LookUp.py
@@ -0,0 +1,12 @@
+# =========================
+# === WHOIS Lookup Tool ===
+# =========================
+
+import whois
+
+from Shared_Tools.Banner import show_banner
+from Shared_Tools.Tables import print_section, print_analysis_complete
+from Shared_Tools.Menu_Utilities import pause
+
+# _________________________________________________________________________________________________
+
