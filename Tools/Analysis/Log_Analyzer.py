97f7b6ce55237c5d0c62dfbe5eb858245cbe4a5b
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-27 20:10:35 +0200

    Feat : Analysis Log_Analyzer.py, Added a counter function for the logs

--- Tools/Analysis/Log_Analyzer.py
+++ Tools/Analysis/Log_Analyzer.py
@@ -8,3 +8,44 @@ from Shared_Tools.Menu_Utilities import pause
 
 # _________________________________________________________________________________________________
 
+def analyze_log(filename):
+
+    try:
+
+        with open(filename, "r") as file:
+            lines = file.readlines()
+
+    except FileNotFoundError:
+
+        print("\nError: File not found :(")
+
+        return
+
+# _________________________________________________________________________________________________
+
+    total_lines = len(lines)
+
+    info = 0
+    warnings = 0
+    errors = 0
+    failed_logins = 0
+
+
+    for line in lines:
+
+        line = line.upper()
+
+        if "INFO" in line:
+            info += 1
+
+        if "WARNING" in line:
+            warnings += 1
+
+        if "ERROR" in line:
+            errors += 1
+
+        if "FAILED LOGIN" in line:
+            failed_logins += 1
+
+# _________________________________________________________________________________________________
+
