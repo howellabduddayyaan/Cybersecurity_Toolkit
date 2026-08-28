d401f955f38ea5f58ec1fac2346a4293e1128e42
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-28 18:26:10 +0200

    Feat : Authentication Login_Hash_System.py, Created file

---
+++ Tools/Authentication/Login_Hash_System.py
@@ -0,0 +1,13 @@
+# =========================
+# === Login Hash System ===
+# =========================
+
+import hashlib
+import os
+
+from Shared_Tools.Banner import show_banner
+from Shared_Tools.Tables import print_section, print_analysis_complete
+from Shared_Tools.Menu_Utilities import pause
+
+# _________________________________________________________________________________________________
+
