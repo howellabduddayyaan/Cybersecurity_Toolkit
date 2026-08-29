568c7cba33e4c539193dfb8b66c24d55bf3dfbfb
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-29 20:13:20 +0200

    Feat : Authentication Password_Manager.py, Created file

---
+++ Tools/Authentication/Password_Manager.py
@@ -0,0 +1,8 @@
+# ========================
+# === Password Manager ===
+# ========================
+
+
+from Shared_Tools.Banner import show_banner
+from Shared_Tools.Tables import print_section, print_analysis_complete
+from Shared_Tools.Menu_Utilities import pause
