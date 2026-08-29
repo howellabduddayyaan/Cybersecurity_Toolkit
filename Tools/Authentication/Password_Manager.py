1bb2ee7bdc7155f54d24a07fe358ab4647861c75
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-08-29 20:23:05 +0200

    Feat : Authentication Password_Manager.py, Added storage vault and master password holder as well as a function to hash the password(hardcodded)

--- Tools/Authentication/Password_Manager.py
+++ Tools/Authentication/Password_Manager.py
@@ -2,7 +2,28 @@
 # === Password Manager ===
 # ========================
 
+import hashlib
+import os
+import string
+import secrets
 
 from Shared_Tools.Banner import show_banner
 from Shared_Tools.Tables import print_section, print_analysis_complete
 from Shared_Tools.Menu_Utilities import pause
+
+# _________________________________________________________________________________________________
+
+VAULT_FILE = "vault.txt"
+
+MASTER_PASSWORD = "Doodle"
+
+
+def hash_password(password):
+
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+MASTER_HASH = hash_password(MASTER_PASSWORD)
+
+
+# _________________________________________________________________________________________________
